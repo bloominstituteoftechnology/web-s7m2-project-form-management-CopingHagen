30d097ed886c72dae52bf47efc2f520caf7d8615
Justyn Silva <justynhagen@gmail.com>
2024-03-22 11:22:57 -0500

    Gave functionality to the submit button and edit button

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -37,6 +37,12 @@ export default function App() {
     // with the data belonging to the member with id 2.
     // On the other hand, if the `editing` state changes back to null
     // then we need to reset the form back to empty values
+    if (editing == null) {
+      setValues(initialValues())
+    } else {
+      const { fname, lname, bio } = members.find(mem => mem.id == editing)
+      setValues({ fname, lname, bio })
+    }
   }, [editing])
 
   const onChange = evt => {
@@ -50,14 +56,20 @@ export default function App() {
     // ✨ Put this function inside a click handler for the <button>Edit</button>.
     // It should change the value of `editing` state to be the id of the member
     // whose Edit button was clicked
+    setEditing(id)
   }
   const submitNewMember = () => {
     // This takes the values of the form and constructs a new member object,
     // which is then concatenated at the end of the `members` state
+    const { fname, lname, bio } = values
+    const newMember = { fname, lname, bio, id: getId() }
+    setMembers([...members, newMember])
+    setValues(initialValues())
   }
   const editExistingMember = () => {
     // ✨ This takes the values of the form and replaces the data of the
     // member in the `members` state whose id matches the `editing` state
+
   }
   const onSubmit = evt => {
     // ✨ This is the submit handler for your form element.
@@ -65,6 +77,12 @@ export default function App() {
     // depending on whether the `editing` state is null or has an id in it.
     // Don't allow the page to reload! Prevent the default behavior
     // and clean up the form after submitting
+    evt.preventDefault()
+    if (editing) {
+      editExistingMember()
+    } else {
+      submitNewMember()
+    } 
   }
   return (
     <div>{/* ✨ Fix the JSX by wiring the necessary values and event handlers */}
@@ -78,7 +96,7 @@ export default function App() {
                   <h4>{mem.fname} {mem.lname}</h4>
                   <p>{mem.bio}</p>
                 </div>
-                <button>Edit</button>
+                <button onClick={() => edit(mem.id)}>Edit</button>
               </div>
             ))
           }
@@ -86,7 +104,7 @@ export default function App() {
       </div>
       <div id="membersForm">
         <h2>{editing ? 'Edit' : 'Add'} a Team Member</h2>
-        <form>
+        <form onSubmit={onSubmit}>
           <div>
             <label htmlFor="fname">First Name </label>
             <input onChange={onChange} value={values.fname} id="fname" type="text" placeholder="Type First Name" />
